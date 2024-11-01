dc90ca08985970c33717a7918c11b646f3b96b37
Mukesh Bhatta <mukeshbhattampb111@gmail.com>
2024-11-01 23:40:37 +0545

    created topdoctors speciality context

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { specialityData } from '../assets/assets_frontend/assets'
+import {Link} from 'react-router-dom'
 const SpecialityMenu = () => {
   return (
-    <div id='speciality' className='flex flex-col gap-3 items-center justify-center mb-10'>
+    <div id='speciality' className='flex flex-col gap-3 items-center justify-center mb-20'>
       <h1 className='text-[20px] md:text-[26px]  font-medium'>Find By Speciality</h1>
       <p className='w-1/2 text-center text-[12px]  text-black text-wrap'>Simply browse through our extensive list of trusted doctors,<br/> schedule your appointment hassle-free.</p>
 
  <div className="flex gap-4 mt-5 justify-center items-center">
         
 
-{specialityData.map((data)=>(
-    <div className="flex flex-col gap-2 justify-center items-center transition-all transform duration-300 hover:-translate-y-2">
+{specialityData.map((data ,index)=>(
+    <Link to={`/doctors/${data.speciality}`} key= {index} className="flex flex-col gap-2 justify-center items-center transition-all transform duration-300 hover:-translate-y-2">
     <img src={data.image} alt="" className='md:w-[88px] w-[60px]' />
    <p className='text-[12px]'>{data.speciality}</p>
-   </div>
+   </Link>
 ))}
 
 </div>
---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,37 @@
+import {React,useContext} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {AppContext} from '../context/AppContext'
+const TopDoctors = () => {
+
+const navigate = useNavigate();
+const {doctors} = useContext(AppContext);
+  return (
+    <div className='w-full mx-auto flex flex-col items-center justify-center gap-4 mb-20 lg:px-28 md:px-20 px-14 '>
+      <h1 className='text-center
+       text-[20px] md:text-[26px]  font-medium'>Top Doctors To Book</h1>
+      <p className='w-1/2 text-center text-[12px]  text-black text-wrap'>Simply browse through our extensive <br/>
+      list of trusted doctors.</p>
+
+      <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 sm:px-0">
+{
+    doctors.slice(0,10).map((doctor,index)=>(
+        <div onClick={()=>navigate(`/appointment/${doctor._id}`)} className="border border-blue-200 rounded-xl transition-all overflow-hidden cursor-pointer duration-300 hover:-translate-y-3 transform" key={index}>
+<img src={doctor.image} alt="" className='bg-blue-50'/>
+<div className="flex items-center gap-2 text-green-600 text-center px-3 pt-3">
+
+<p className='w-[7px] h-[7px] rounded-full bg-green-600  '></p><p className='text-[13px]'>Available</p>
+</div>
+<p className='text-gray-900 text-md font-medium px-3'>{doctor.name}</p>
+<p className='text-gray-900 text-sm px-2 pb-2 '>{doctor.speciality}</p>
+
+        </div>
+    ))
+}
+
+      </div>
+      <button onClick={()=>{navigate('/doctors');scrollTo(0,0)}} className='border rounded-[20px] px-6 py-1 bg-blue-50'>More</button>
+    </div>
+  )
+}
+
+export default TopDoctors
