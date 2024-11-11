f8c543036698a31d6b504dd97d25a362ec0f2a15
Mukesh Bhatta <mukeshbhattampb111@gmail.com>
2024-11-12 00:59:22 +0545

    completed my profile page

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,21 @@
 import React, { useState } from 'react'
 import {assets} from '../assets/assets_frontend/assets'
 const MyProfile = () => {
-
-  const [isEdit,setIsEdit] = useState(true);
+const handleFile= (e)=>{
+const file =e.target.files[0];
+if(file)
+{
+  const reader = new FileReader();
+  reader.onloadend = ()=>{
+    setUserData((prev)=>({
+      ...prev,
+image:reader.result
+    }))
+  }
+  reader.readAsDataURL(file);
+}
+}
+  const [isEdit,setIsEdit] = useState(false);
  
   const [userData,setUserData] = useState({
     name:"Edward Reigns",
@@ -22,38 +35,49 @@ const MyProfile = () => {
   return (
     
     <div className='max-w-[80vw] mx-auto mb-20'>
-      <img src={userData.image} alt="" />
+      {
+        isEdit ?
+        <input type='file' accept='image/*' onChange={handleFile} />
+:
+        <img src={userData.image} alt="" className='w-40  mt-10' />
+}
 <div className="border-b border-b-gray-500 py-4">
 
 
       { isEdit?
-      <input type="text" placeholder='Name' onChange={(e)=>setUserData(prev=>({...prev,name:e.target.value}))} value={userData.name}/>
+      <input type="text" placeholder='Name' onChange={(e)=>setUserData(prev=>({...prev,name:e.target.value}))} value={userData.name} required className='bg-gray-100 text-center'/>
       :
-      <p>{userData.name}</p>
+      <p className='text-[20px] text-gray-600'>{userData.name}</p>
     }
     </div>
 
-<div className="">
-  <p>Contact Information </p>
-  <div className="">
+<div className="mt-4 mb-4 flex flex-col gap-4">
+  <p className='text-gray-500 underline underline- text-[16px]'>Contact Information </p>
+  <div className="flex gap-12">
 
-<p>Email:{userData.email}</p>
-<p>Phone:</p>
+<p className='text-gray-600 font-bold text-[14px]'>Email:</p>
+<p className='text-blue-600 cursor-pointer text-[14px]'>{userData.email}</p>
+ </div>
+<div className="flex gap-10">
+
+<p className='text-gray-600 font-bold text-[14px]'>Phone:</p>
 {isEdit ?
-<input type="tel" value={userData.phone} onChange={(e)=>setUserData(prev=>({...prev,phone:e.target.value}))} placeholder='phone' />
+<input type="tel" value={userData.phone} onChange={(e)=>setUserData(prev=>({...prev,phone:e.target.value}))} placeholder='phone' className='bg-gray-100 text-center' />
 :
-<p>{userData.phone}</p>
+<p className='text-gray-500 text-[14px]'>{userData.phone}</p>
 }
+</div>
+<div className="flex gap-8">
 
-<p>Address:</p>
+<p className='text-gray-600 font-bold text-[14px]'>Address:</p>
 {
   isEdit ?
-  <div className="">
+  <div className="flex flex-col gap-2">
 
-  <input type="text" value={userData.address.line1} onChange={(e)=>setUserData(prev=>({...prev,address:{...prev.address,line1:e.target.value}}))} placeholder='phone' />
- <input type='text' value={userData.address.line2} onChange={(e)=>setUserData(prev=>({...prev,address:{...prev.address,line2:e.target.value}}))} />
+  <input type="text" value={userData.address.line1} onChange={(e)=>setUserData(prev=>({...prev,address:{...prev.address,line1:e.target.value}}))} placeholder='phone' className='bg-gray-100 text-center' />
+ <input type='text' value={userData.address.line2} onChange={(e)=>setUserData(prev=>({...prev,address:{...prev.address,line2:e.target.value}}))} className='bg-gray-100 text-center' />
   </div>:
-  <p>{userData.address.line1}
+  <p className='text-gray-500  text-[14px]'>{userData.address.line1}
   <br/>
   {userData.address.line2}
   </p>
@@ -62,26 +86,30 @@ const MyProfile = () => {
 
   </div>
 
-<div className="">
-  <p>Basic Information</p>
-<div className="">
-  <p>Gender: </p>
+<div className="flex flex-col gap-2 mb-8">
+  <p className='text-gray-500 underline underline- text-[16px] mb-2 '>Basic Information</p>
+<div className="flex gap-10">
+  <p className='text-gray-600 font-bold text-[14px]'>Gender: </p>
   {
 
 isEdit ?
-<select value={userData.gender} onChange={(e)=>setUserData(prev=>({...prev,gender:e.target.value}))}>
+<select value={userData.gender} onChange={(e)=>setUserData(prev=>({...prev,gender:e.target.value}))} className='bg-gray-100 text-center'>
 <option value='Male' >Male</option>
 <option value='Female'>Female</option>
 </select>
 :
-<p>{userData.gender}</p>
+<p className='text-gray-500 text-[14px]'>{userData.gender}</p>
   }
+
+  </div>
+  <div className="flex gap-6">
+    
   <p>Birthday:</p>
   {
     isEdit ?
-    <input type='date' value={userData.dob} onChange={(e)=>setUserData(prev=>({...prev,dob:e.target.value}))}/>
+    <input type='date' value={userData.dob} onChange={(e)=>setUserData(prev=>({...prev,dob:e.target.value}))} className='bg-gray-100 text-center'/>
     :
-    <p>{userData.dob}</p>
+    <p className='text-gray-500 text-[14px]'>{userData.dob}</p>
   }
 </div>
 
