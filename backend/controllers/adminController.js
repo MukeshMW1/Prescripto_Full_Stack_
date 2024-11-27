9e5d80af565e0d19193448e4dd4a8df5420f4b44
Mukesh Bhatta <mukeshbhattampb111@gmail.com>
2024-11-27 18:28:45 +0545

    middleware model controller config

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,12 @@
+
+
+
+//API for adding Dotors
+const addDocot = async (req,res)=>{
+    try {
+        const {name,email,password,image,speciality,degree,experience,fees,about,address} = req.body
+        
+    } catch (error) {
+        
+    }
+    }
