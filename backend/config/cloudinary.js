35b9201baf2ab287e48705010fa7c03a4e103ac0
Mukesh Bhatta <mukeshbhattampb111@gmail.com>
2024-12-04 21:07:24 +0545

    mongoose version updated

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -5,8 +5,8 @@ const connectCloudinary = async ()=>
 {
     cloudinary.config({
 cloud_name:process.env.CLOUDINARY_NAME,
-api_key:process.env.CLOUDINRY_API_KEY,
-api_key:process.env.CLOUDINRY_SECRET_KEY,
+api_key:process.env.CLOUDINARY_API_KEY,
+api_secret:process.env.CLOUDINARY_SECRET_KEY,
     })
 }
 
